1c44c8602bbd630df60d7bb66bb28ca8060993c6
Cliff Lu <clifflu@gmail.com>
2011-12-29 00:33:14 +0800

    修改測試查詢日期

--- index.php
+++ index.php
@@ -7,11 +7,15 @@ require_once('leech/pcc111228.php');
 echo pcc111228::bite(
 	array(
 		'POST' => array(
+			'tenderStartDate' => '100/12/28',
+			'tenderEndDate' => '100/12/28',
 			'tenderName'=>'中文',
 		),
 	),
 	array(
 		'POST' => array(
+			'tenderStartDate' => '100/12/28',
+			'tenderEndDate' => '100/12/28',
 			'tenderName' => '地理',
 		),
 	)
