6763819f8059a577ae17e5c5f29b56d3594993c6
Cliff Lu <clifflu@gmail.com>
2011-12-29 14:58:13 +0800

    now deal with the UI

--- src/index.php
+++ src/index.php
@@ -1,28 +1,5 @@
 <?php
 
-require_once('config.php');
 
-echo pcc::bite(
-	array(
-		'POST' => array(
-			'tenderStartDate' => '100/12/28',
-			'tenderEndDate' => '100/12/28',
-			'tenderName'=>'中文',
-		),
-	),
-	array(
-	        'POST' => array(
-			            'tenderName'=>'中文',
-						        ),
-								    ),
-	array(
-		'POST' => array(
-			'tenderStartDate' => '100/12/28',
-			'tenderEndDate' => '100/12/28',
-			'tenderName' => '地理',
-		),
-	)
-);
 
-echo pcc::class_info();
 ?>
---
+++ src/result.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once('common.php');
+
+echo pcc::bite(
+	array(
+		'POST' => array(
+			'tenderStartDate' => '100/12/28',
+			'tenderEndDate' => '100/12/28',
+			'tenderName'=>'中文',
+		),
+	),
+	array(
+	        'POST' => array(
+			            'tenderName'=>'中文',
+						        ),
+								    ),
+	array(
+		'POST' => array(
+			'tenderStartDate' => '100/12/28',
+			'tenderEndDate' => '100/12/28',
+			'tenderName' => '地理',
+		),
+	)
+);
+
+echo pcc::class_info();
+?>
