2a50cd0c64f5f431b2ff59f144beb5cac1052ebd
Cliff Lu <clifflu@gmail.com>
2011-12-31 16:09:31 +0800

    fix proto on imlearning website

--- tpl/footer.php
+++ tpl/footer.php
@@ -1,7 +1,7 @@
 			<div class="clear">&nbsp;</div>
 			<div id="footer">
 				<div class="author">
-					<a href="www.imlearning.com.tw">學思科技 &trade;</a>
+					<a href="http://www.imlearning.com.tw">學思科技 &trade;</a>
 					:: 
 					<a href="mailto:clifflu@gmail.com">Cliff Lu (clifflu)</a>
 				</div>
